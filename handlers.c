aa0cd00d19eb99ebcc222fc7682a6e7f8a2b4e21
Brod8362 <brod8362@gmail.com>
2021-02-20 10:03:00 -0600

    Show preview image, still needs scaling

--- handlers.c
+++ handlers.c
@@ -7,12 +7,15 @@ void file_selected(GtkTreeView* tree_view, GtkTreePath* tree_path, GtkTreeViewCo
     GtkTreeSelection* selected = gtk_tree_view_get_selection(tree_view);
     GtkTreeModel* model = gtk_tree_view_get_model(tree_view);
     GtkTreeIter iter;
-    const char* name;
+    const char* filename;
 
     //load selected row into iter
     gtk_tree_selection_get_selected(selected, &model, &iter);
-    gtk_tree_model_get(model, &iter, 0, &name, -1);
+    gtk_tree_model_get(model, &iter, 0, &filename, -1);
 
     //first, set the image label 
-    gtk_label_set_text(info->filename_label, name);
+    gtk_label_set_text(info->filename_label, filename);
+
+    //then set the image
+    gtk_image_set_from_file(info->image, filename);
 }
